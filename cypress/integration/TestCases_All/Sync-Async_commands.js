30b413776cb896bf75bc9eda999c2f069408687f
Tejas <work.tejas1@gmail.com>
2021-09-02 02:33:51 +0200

    fixtures and custom commands
    - custom commands
    - fixtures
    - advance custom commands

--- cypress/integration/TestCases_All/Sync-Async_commands.js
+++ cypress/integration/TestCases_All/Sync-Async_commands.js
@@ -3,7 +3,8 @@
 describe('Sync and Async code', () => {
     //nested then is okay to use
     it('sync and promise', function () {
-        cy.visit("https://automationteststore.com/")
+        //cy.visit("https://automationteststore.com/")
+        cy.visit("/") //cypress.json baseurl
         cy.get(".categorymenu").contains("Makeup").click()
         cy.get("h1 .maintext").then(($eleText) => {
             const text = $eleText.text()
