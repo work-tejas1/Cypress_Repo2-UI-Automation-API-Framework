5c21498a8a389e413fcfe77d6dc6db0f6f39d690
Tejas <work.tejas1@gmail.com>
2021-09-04 03:49:46 +0200

    checked cypress.json config
    checked
    "videoCompression": false,
     "trashAssetsBeforeRuns" : true,
    package.json TestCases_All update

--- cypress/integration/TestCases_All/ChaiAssertations.js
+++ cypress/integration/TestCases_All/ChaiAssertations.js
@@ -7,12 +7,12 @@ describe('Chai Assertations ', () => {
         // https://docs.cypress.io/guides/references/assertions#Chai-jQuery
         //cy.contains("Makeup").should("have.text", "Makeup")
         cy.visit("https://automationteststore.com/index.php?rt=content/contact")
-        cy.get("#ContactUsFrm_email").should("have.attr", "name" , "email")
+        cy.get("#ContactUsFrm_email").should("have.attr", "name", "email")
     })
 
     it('document() ,  title() command', function () {
         cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
-        cy.document().should("have.property", "charset").and("eq" , "UTF-8")
+        cy.document().should("have.property", "charset").and("eq", "UTF-8")
         cy.title().should("include", "Contact Us")
     })
 
