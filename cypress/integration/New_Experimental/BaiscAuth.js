92bc7782bb3ca8f39724ff7179c6c23ac8b2eff5
Tejas <51386360+work-tejas1@users.noreply.github.com>
2022-03-23 21:21:46 +0100

    basic auth formatted
    basic auth formatting using prettier

--- cypress/integration/New_Experimental/BaiscAuth.js
+++ cypress/integration/New_Experimental/BaiscAuth.js
@@ -1,15 +1,10 @@
 /// <reference types="cypress" />
 
 describe('Basic Auth login', () => {
-
-    it('basic auth login Test', () => {
-       
-        cy.clearLocalStorage()
-        cy.clearCookies()
-        cy.visit("https://admin:admin@the-internet.herokuapp.com/basic_auth")
-        cy.contains("Basic Auth").should("not.be.null")
-    })
-
-    
-
+  it('basic auth login Test', () => {
+    cy.clearLocalStorage()
+    cy.clearCookies()
+    cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth')
+    cy.contains('Basic Auth').should('not.be.null')
+  })
 })
